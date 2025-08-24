518ae32d56bb71bdfc386b7df5f9399ed1af59ff
Ahmed Hussain <ahmedhussainnow@gmail.com>
2025-08-24 10:58:06 +0600

    init cloudinary

--- file_system_01/stream.js
+++ file_system_01/stream.js
@@ -7,7 +7,7 @@ const filePath2 = path.join(__dirname, "files", "stream2.txt");
 const rs = fs.createReadStream(filePath, { encoding: "utf-8" });
 const ws = fs.createWriteStream(filePath2);
 
-// async
+// async, rs listens to `data` emit event
 rs.on("data", (dataChunk) => {
   ws.write(dataChunk);
 });
